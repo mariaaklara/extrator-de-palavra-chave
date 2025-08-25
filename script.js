d355e16689ec7548337706b48becde3a77558ba8
mariaaklara <denck.maria@escola.pr.gov.br>
2025-08-25 08:45:27 -0300

    fgv

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.query Selector (*#botao-palavrachave*):
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave):
+function mostraPalavrasChave0 {
+const texto = document.query Selector (*#entrada-de-texto*).value;
+const campoResultado = document. query Selector (*#resultado-palavrachave"):
+const palavrasChave = processaTexte(texto):
+campoResultado.textContent = palavrasChave.join(*. ):
+}
