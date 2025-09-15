c4b85aac32bdad6528fac43fade973b26b0892b8
mariaaklara <denck.maria@escola.pr.gov.br>
2025-09-15 08:50:36 -0300

    xdg

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u); 
+function processa Texto (texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = [];
+    for(let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias [i]++;
+            }
+        }   
+    }     
+    console.log(frequencias);
+    return palavras;
+}    
+    
 
-  return palavras;
-}
+    
+
