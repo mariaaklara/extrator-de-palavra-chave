a8572c78a51d7609f1836df913ba35605836ce9b
mariaaklara <denck.maria@escola.pr.gov.br>
2025-09-15 08:33:03 -0300

    dg

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const botaoMostraPalavras = document.querySelector ("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-const texto = document.query Selector (*#entrada-de-texto*).value;
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-const campoResultado = document.querySelector ("#resultado-palavrachave"):
+  const campoResultado = document.querySelector("#resultado-palavrachave");
 
-const palavrasChave = processaTexte(texto);
+  const palavrasChave = processaTexto(texto);
 
-campoResultado.textContent = palavrasChave.join(",");
+  campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto){
-    let palavras =texto.split(/\P{L}+/u);
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u); 
 
-    return palavras;
+  return palavras;
 }
